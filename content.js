ce6464c3e939222329ee08d8e1521e2f5a63dd82
王嘉巍 <906789088@qq.com>
2019-05-14 16:49:16 +0800

    how to use webpack and webpack-cli in project

--- content.js
+++
@@ -1,9 +0,0 @@
-function Content() {
-  var dom = document.getElementById("root");
-  var content = document.createElement("div")
-  content.innerText = "sidebar"
-  dom.append(content)
-}
-
-// export default Content;
-module.exports = Content;
