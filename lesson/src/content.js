66f476180869156fe1345df3513b3aa09ac7a858
王嘉巍 <906789088@qq.com>
2019-05-15 00:33:55 +0800

    create webpack.config.js and build npm script become easily npx webapck

---
+++ lesson/src/content.js
@@ -0,0 +1,9 @@
+function Content() {
+  var dom = document.getElementById("root");
+  var content = document.createElement("div")
+  content.innerText = "sidebar"
+  dom.append(content)
+}
+
+// export default Content;
+module.exports = Content;
---
+++ lesson/src/index.js
@@ -0,0 +1,9 @@
+var Header = require('./header');
+var Sidebar = require('./sidebar');
+var Content = require('./content');
+new Header();
+new Sidebar();
+new Content();
+
+
+
