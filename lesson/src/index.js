8e745ace097159f1aa341303c67ba5f280727d40
王嘉巍 <906789088@qq.com>
2019-06-01 10:13:32 +0800

    import babel/polyfill in webpack Es6 transport es5 language

--- lesson/src/index.js
+++ lesson/src/index.js
@@ -1,23 +1,9 @@
-// import './style.css';
-import counter from './counter';
-import number from './number';
-counter()
-number()
-if (module.hot) {
-  module.hot.accept('./number.js', () => {
-    document.body.removeChild(document.getElementById('number'));
-    number()
-  })
-}
-// var btn = document.createElement('button');
-// btn.innerHTML = '新增';
-// document.body.appendChild(btn);
-// btn.onclick = function() {
-//   var div = document.createElement('div');
-//   div.innerHTML = 'item';
-//   document.body.appendChild(div);
-// }
+import "@babel/polyfill";
+const arr = [
+  new Promise(() => {}),
+  new Promise(() => {})
+]
 
-
-
-
+arr.map(item => {
+  console.log(item);
+})
--- lesson/webpack.config.js
+++ lesson/webpack.config.js
@@ -16,6 +16,15 @@ module.exports = {
   },
   module: {
     rules: [{
+       test: /\.js$/, 
+        exclude: /node_modules/, 
+        loader: 'babel-loader',
+        options: {
+          presets: [['@babel/preset-env', {
+            useBuiltIns: 'usage'
+          }]]
+        }
+      }, {
       test: /\.(jpg|png|gif)$/,
       use: {
         loader: 'url-loader',
