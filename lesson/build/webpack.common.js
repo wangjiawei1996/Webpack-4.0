50d172f7552ed31f0e9b7ea2f60d0a01dbcbc06e
王嘉巍 <906789088@qq.com>
2019-06-11 15:47:07 +0800

    catcheFroups vendors build filename vendors.js and chunks become all

--- lesson/build/webpack.common.js
+++ lesson/build/webpack.common.js
@@ -55,7 +55,21 @@ module.exports = {
   ],
   optimization: {
     splitChunks: {
-      chunks: 'all'
+      chunks: "all",
+      minSize: 30000,
+      minChunks: 1,
+      maxAsyncRequests: 5,
+      maxInitialRequests: 3,
+      automaticNameDelimiter: '~',
+      name: true,
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          filename: 'vendors.js',
+        },
+        default: false
+      }
     }
   },
   output: {
--- lesson/src/index.js
+++ lesson/src/index.js
@@ -1,10 +1,14 @@
-function getComponent() {
-  return import('lodash').then(({ default:_ }) => {
-    var element = document.createElement('div');
-    element.innerHTML = _.join(['Dell', 'Lee'], '_');
-    return element
-  })
-}
-getComponent().then(element => {
-  document.body.appendChild(element)
-})
+import _ from 'lodash'
+var element = document.createElement('div')
+element.innerHTML = _.join(['Jia', 'Wei'],'-');
+document.body.appendChild(element)
+// function getComponent() {
+//   return import(/* webpackChunkName:"lodash" */ 'lodash').then(({ default:_ }) => {
+//     var element = document.createElement('div');
+//     element.innerHTML = _.join(['Dell', 'Lee'], '-');
+//     return element
+//   })
+// }
+// getComponent().then(element => {
+//   document.body.appendChild(element)
+// })
