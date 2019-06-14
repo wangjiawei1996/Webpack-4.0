c0848bfd81bad327f4a963fc75510089be018192
王嘉巍 <906789088@qq.com>
2019-06-14 10:01:04 +0800

    yarn lazy loading and chunk set all to webpack index.js

--- lesson/build/webpack.common.js
+++ lesson/build/webpack.common.js
@@ -55,25 +55,7 @@ module.exports = {
   ],
   optimization: {
     splitChunks: {
-      chunks: "all",
-      minSize: 30000,
-      minChunks: 1,
-      maxAsyncRequests: 5,
-      maxInitialRequests: 3,
-      automaticNameDelimiter: '~',
-      name: true,
-      cacheGroups: {
-        vendors: {
-          test: /[\\/]node_modules[\\/]/,
-          priority: -10,
-          filename: 'vendors.js',
-        },
-        default: {
-          priority: -20,
-          reuseExistingChunk: true,
-          filename: 'common.js'
-        }
-      }
+      chunks: "all"
     }
   },
   output: {
--- lesson/src/index.js
+++ lesson/src/index.js
@@ -1,16 +1,11 @@
-// import test from './test'
-// console.log(test.name)
-import _ from 'lodash'
-var element = document.createElement('div')
-element.innerHTML = _.join(['Jia', 'Wei'],'-');
-document.body.appendChild(element)
-// function getComponent() {
-//   return import(/* webpackChunkName:"lodash" */ 'lodash').then(({ default:_ }) => {
-//     var element = document.createElement('div');
-//     element.innerHTML = _.join(['Dell', 'Lee'], '-');
-//     return element
-//   })
-// }
-// getComponent().then(element => {
-//   document.body.appendChild(element)
-// })
+async function getComponent() {
+  const { default: _} = await import(/* webpackChunkName:"lodash" */ 'lodash');
+  const element = document.createElement('div');
+  element.innerHTML = _.join(['Dell', 'Lee'], '-');
+  return element
+}
+document.addEventListener('click', () => {
+  getComponent().then(element => {
+    document.body.appendChild(element)
+  })
+})
