b423b9cabf1eece4979b92d7ba5b58fd223b976e
王嘉巍 <906789088@qq.com>
2019-06-16 16:57:11 +0800

    webpack analyse、 preloading、 and prefetching magic become true

--- lesson/src/index.js
+++ lesson/src/index.js
@@ -1,11 +1,6 @@
-async function getComponent() {
-  const { default: _} = await import(/* webpackChunkName:"lodash" */ 'lodash');
-  const element = document.createElement('div');
-  element.innerHTML = _.join(['Dell', 'Lee'], '-');
-  return element
-}
+
 document.addEventListener('click', () => {
-  getComponent().then(element => {
-    document.body.appendChild(element)
+  import(/* webpackPrefetch: true */'./click').then(({default: func}) => {
+    func()
   })
 })
