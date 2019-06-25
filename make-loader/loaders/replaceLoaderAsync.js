d13b8436ea63ae670144e27314a24440c3bf2227
王嘉巍 <906789088@qq.com>
2019-06-25 23:50:38 +0800

    set a plugin in webpack using constructor to this function at end of webpack.config.js new this plugin

--- make-loader/loaders/replaceLoaderAsync.js
+++ make-loader/loaders/replaceLoaderAsync.js
@@ -1,10 +1,5 @@
 const loaderUtils = require('loader-utils');
 module.exports = function(source) {
-  if (Node全局变量 === '中文') {
-    source.replace('{{title}}','中文标题')
-  } else {
-    source.replace('{{title}}', '中文标题') 
-  }
   const options = loaderUtils.getOptions(this);
   const callback = this.async();
   setTimeout(() => {
---
+++ plugin/plugins/copyright-webpack-plugin.js
@@ -0,0 +1,9 @@
+class CopyRightWebpackPlugin {
+  constructor() {
+    console.log('插件别使用了 ')
+  }
+  apply(complier) {
+
+  }
+}
+module.exports = CopyRightWebpackPlugin;
