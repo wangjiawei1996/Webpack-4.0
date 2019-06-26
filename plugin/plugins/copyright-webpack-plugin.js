56d809fd222811bf91728f60d6c17db27ffec4e0
王嘉巍 <906789088@qq.com>
2019-06-27 00:09:34 +0800

    using node debug to debugger thisv console to webpack

--- plugin/plugins/copyright-webpack-plugin.js
+++ plugin/plugins/copyright-webpack-plugin.js
@@ -1,6 +1,10 @@
 class CopyRightWebpackPlugin {
   apply(complier) {
+    complier.hooks.compile.tap('CopyRightWebpackPlugin', (compilation) => {
+      console.log('compiler')
+    })
     complier.hooks.emit.tapAsync('CopyRightWebpackPlugin ',(compilation, cb) => {
+      debugger;
       compilation.assets['copyright.txt'] = {
         source: function() {
           return 'copyright by jiawei'
