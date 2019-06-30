ccafafd4ee658ea1093643f582b48ba9862d30db
王嘉巍 <906789088@qq.com>
2019-06-30 16:43:55 +0800

    import babel parser and core to analyser filename to presets babel

--- bundler/bundler.js
+++ bundler/bundler.js
@@ -1,10 +1,30 @@
 const fs = require('fs');
+const path = require('path');
 const parser = require('@babel/parser');
+const traverse = require('@babel/traverse').default;
+const babel = require('@babel/core');
 const moduleAnalyser = (filename) => {
   const content = fs.readFileSync(filename, 'utf-8');
   const ast = parser.parse(content, {
     sourceType: 'module'
   });
-  console.log(ast.program.body)
+  const dependencies = {};
+  traverse(ast, {
+    ImportDeclaration({ node }) {
+      const dirname = path.dirname(filename);
+      const newFile = './' + path.join(dirname, node.source.value)
+      dependencies[node.source.value] = newFile
+    }
+  });
+  const { code } = babel.transformFromAst(ast, null, {
+    presets: ["@babel/preset-env"]
+  });
+  console.log(code);
+  return {
+    filename,
+    dependencies,
+    code 
+  }
 }
-moduleAnalyser('./src/index.js')
+const moduleInfo = moduleAnalyser('./src/index.js');
+console.log(moduleInfo)
